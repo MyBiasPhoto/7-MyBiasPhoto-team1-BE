bb225ba83f9ac3d1fd9f55491becf0a028b1c07c
Lee Yujin <yuwls1102@gmail.com>
2025-08-07 00:37:15 +0900

    refactor: 분리된 공통 로직 기반으로 get-my-gallery, get-my-market API에 트랜잭션 적용 #10 #11

--- src/modules/userCard/routes.js
+++ src/modules/userCard/routes.js
@@ -1,6 +1,8 @@
 import UserCardController from './controller.js';
 import UserCardService from './service.js';
 import UserCardRepository from './repository.js';
+import UserRepository from '../user/repository.js';
+import UserCardTransaction from './transaction.js';
 import { Router } from 'express';
 import { verifyAccessToken } from '../../common/middleware/verifyAccessToken.js';
 import { getMyGalleryListSchema } from './schema/getMyGalleryListSchema.js';
@@ -9,7 +11,13 @@ import { validate } from '../../common/middleware/validate.js';
 const userCardRouter = Router({ mergeParams: true });
 
 const userCardRepository = new UserCardRepository();
-const userCardService = new UserCardService(userCardRepository);
+const userRepository = new UserRepository();
+const userCardTransaction = new UserCardTransaction(userCardRepository);
+const userCardService = new UserCardService(
+  userCardRepository,
+  userCardTransaction,
+  userRepository
+);
 const userCardController = new UserCardController(userCardService);
 
 userCardRouter.get(
