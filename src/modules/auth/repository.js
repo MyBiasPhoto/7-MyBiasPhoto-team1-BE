bfbe33e960630d26cd9af475f16b564eb98a2fd3
Lee Yujin <yuwls1102@gmail.com>
2025-08-07 11:10:09 +0900

    refactor: 사용자 관련 DB로직을 AuthRepository에서 UserRepository로 이동

--- src/modules/auth/repository.js
+++ src/modules/auth/repository.js
@@ -1,23 +1,5 @@
 import { prisma } from '../../common/utils/prisma.js';
 
-class AuthRepository {
-  findUserByEmail = async (email) => {
-    const existingEmail = await prisma.user.findUnique({
-      where: {
-        email: email,
-      },
-    });
-
-    return existingEmail;
-  };
-
-  createUser = async (userData) => {
-    const newUser = await prisma.user.create({
-      data: userData,
-    });
-
-    return newUser;
-  };
-}
+class AuthRepository {}
 
 export default AuthRepository;
--- src/modules/user/repository.js
+++ src/modules/user/repository.js
@@ -1,6 +1,24 @@
 import { prisma } from '../../common/utils/prisma.js';
 
 class UserRepository {
+  findUserByEmail = async (email) => {
+    const existingEmail = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    return existingEmail;
+  };
+
+  createUser = async (userData) => {
+    const newUser = await prisma.user.create({
+      data: userData,
+    });
+
+    return newUser;
+  };
+
   findUserById = async (userId) => {
     const user = await prisma.user.findUnique({
       where: { id: userId },
