143a2fcec6b120864429f18c4dd3260935244414
Woojin <jwj3171@gmail.com>
2025-08-09 20:54:37 +0900

    fix(buySale): 판매자 포인트 증가 로직 및 동시성 검증 추가 #3

--- src/common/utils/throwApiErrors.js
+++ src/common/utils/throwApiErrors.js
@@ -4,4 +4,7 @@ import { ApiError } from './apiError.js';
 
 export const throwApiError = (code, message, status = 400) => {
   throw new ApiError(code, message, status);
+  //code NOT_ENOUGH_MINERALS
+  // message : 프론트에서 보여질 내용 돈이부족하여 구매에 실패했습니다
+  // 400 (bad request)
 };
